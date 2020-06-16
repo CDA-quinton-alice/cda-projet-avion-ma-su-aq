7070fc2236aa0408f53d3e4f708f74dffe4aeca3
Samuel-umek <samuel.umek@gmail.com>
2020-06-16 09:40:44 +0200

    test encoding

--- cda-projet-avion-ma-su-aq/src/test/test.java
+++ cda-projet-avion-ma-su-aq/src/test/test.java
@@ -3,6 +3,7 @@ package test;
 public class test {
 
 	public static void main(String[] args) {
+		//commentaire nul éééé_èéào)àç_
 		System.out.println("OK mathieu ééééééééééééééééééééé");
 		System.out.println("blablabla èèèèèèèèèèè");
 	}
