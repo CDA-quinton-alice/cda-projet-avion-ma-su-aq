f2ae8d9d6dc4b0bd9209ab7e908398edb6d442e9
Samuel-umek <samuel.umek@gmail.com>
2020-06-17 14:19:00 +0200

    Gestion des collisions 80% fini

--- cda-projet-avion-ma-su-aq/src/controller/Collision.java
+++ cda-projet-avion-ma-su-aq/src/controller/Collision.java
@@ -1,11 +1,31 @@
 package controller;
 
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
 import model.Meteorite;
+import model.MeteoriteSimple;
 import model.Player;
 
 public class Collision {
 	
-	public static boolean checkCollision(Meteorite m) {
-		return Player.getInstance().getBounds().intersects(m.getBounds());
+	private static ArrayList<MeteoriteSimple> test = new ArrayList<MeteoriteSimple>() {
+		{
+			add(new MeteoriteSimple(10,10));
+		}
+	};
+
+	public static boolean checkCollision() {
+		ArrayList<MeteoriteSimple> meteorites = test;
+		
+		for(MeteoriteSimple m : meteorites) {
+			if(m.getBounds().intersects(Player.getInstance().getBounds())) {
+				//Player.isHirt(m.getDamages());
+				Player.isHit(1);
+				meteorites.remove(m);
+				return true; 
+			}
+		}
+		return false;
 	}
 }
