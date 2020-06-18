feb0d6fcb5281ed39dacfcd6dd646ddae48b9ff3
Samuel-umek <samuel.umek@gmail.com>
2020-06-18 13:51:41 +0200

    Système de points fini - Demande de pseudo

--- cda-projet-avion-ma-su-aq/src/view/Frame.java
+++ cda-projet-avion-ma-su-aq/src/view/Frame.java
@@ -3,31 +3,44 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
+import controller.Collision;
+import model.Meteorite;
+import model.MeteoriteSimple;
 import model.Player;
 
-public class Frame extends JLayeredPane {
+public class Frame extends JLayeredPane{
 
 	private Image fondGalaxie;
 	public static final int WIDTH = 1200;
 	public static final int HEIGHT = 700;
 	private File listeScore;
 
-	public Frame(String JeuBattleFront, String pNom) {
+	public Frame(String JeuBattleFront) {
 		
 		setupFichierScore();
 		setupImage();
+		demanderNom();
 		this.setPreferredSize(new Dimension(1200,700));
 		
 		ContentPane p = new ContentPane(fondGalaxie);
@@ -36,10 +49,9 @@ public class Frame extends JLayeredPane {
 		
 		//Player
 		Player joueur = Player.getInstance();
-		Player.setName(pNom);
-		SpriteJoueur cocaJoueur = new SpriteJoueur(Player.getSprite(), Player.getPosX(), Player.getPosY(),joueur.getWidth(), joueur.getHeight());
-		cocaJoueur.setOpaque(false);
-		cocaJoueur.setSize(this.getPreferredSize());
+		SpriteJoueur joueurPan = new SpriteJoueur(Player.getSprite(), Player.getPosX(), Player.getPosY(),joueur.getWidth(), joueur.getHeight());
+		joueurPan.setOpaque(false);
+		joueurPan.setSize(this.getPreferredSize());
 		
 		//Vies
 		ViewVies vv = ViewVies.getInstance();
@@ -54,13 +66,13 @@ public class Frame extends JLayeredPane {
 		ViewPoints vp = ViewPoints.getInstance();
 		vp.setForeground(Color.WHITE);
 		infos.add(vp, BorderLayout.EAST);
-		
+		infos.setOpaque(false);
+		ecrireScores();
 		
 		//Ajout des panel
 		this.add(p,JLayeredPane.DEFAULT_LAYER);
-		this.add(cocaJoueur,JLayeredPane.PALETTE_LAYER);
+		this.add(joueurPan, JLayeredPane.PALETTE_LAYER);
 		this.add(infos, JLayeredPane.MODAL_LAYER);
-		
 		
 		JFrame frame = new JFrame(JeuBattleFront);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -89,7 +101,8 @@ public class Frame extends JLayeredPane {
 				}
 				
 				FileWriter myWriter = new FileWriter(listeScore.getAbsoluteFile());
-				myWriter.write("Ca fonctionne !");
+				myWriter.flush();
+				myWriter.write("Liste des scores ");
 				myWriter.close();
 				
 			}
@@ -112,14 +125,31 @@ public class Frame extends JLayeredPane {
 	}
 	
 	public void ecrireScores() {
-		
+		try {
+			FileWriter mfw = new FileWriter(listeScore.getAbsoluteFile(), true);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+			mfw.write("\n"+Player.getInstance().getName()+";"+Player.getInstance().getPoints().getPoints()+";"+sdf.format(new Date()));
+			mfw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void demanderNom() {
+		String nom = "";
+		while(!nom.matches("[a-zA-Z\\s]{3,6}")) {
+			nom = (String)JOptionPane.showInputDialog(
+					this,
+					"Entrez votre nom pour les scores :",
+					"Scores",
+					JOptionPane.QUESTION_MESSAGE
+				);
+		}
+		
+		Player.setName(nom);
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//		System.out.print("Veuillez entrer votre pseudo : ");
-//		String nom = sc.nextLine();
-		String nom = "Test";
-		new Frame("BattleFront",nom);
+		new Frame("BattleFront");
 	}
 }
